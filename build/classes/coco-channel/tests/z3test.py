c4022b90dad3f4b921bb327bd8321f3470ddb231
Seemanta Saha <seemantasaha@gmail.com>
2022-02-18 13:59:22 -0800

    Removing uncessary code for command line version

--- build/classes/coco-channel/tests/z3test.py
+++
@@ -1,9 +0,0 @@
-from z3 import *
-
-if __name__ == "__main__":
-	x = Int('x')
-	y = Int('y')
-	s = Solver()
-	s.add(x + y > 5)
-	print(s.check())
-	print(s.model())
--- sv-comp-modified-benchmarks/jbmc-regression-modified/array1/what_not.java
+++
@@ -1,1 +0,0 @@
-class what_not {};
